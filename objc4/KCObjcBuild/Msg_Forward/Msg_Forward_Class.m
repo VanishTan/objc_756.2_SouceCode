2587934ce98caef30871a9090e5bee4529e0a655
谭诚 <tc.7380173@163.com>
2021-03-02 10:30:48 +0800

    1

--- objc4/KCObjcBuild/Msg_Forward/Msg_Forward_Class.m
+++ objc4/KCObjcBuild/Msg_Forward/Msg_Forward_Class.m
@@ -9,27 +9,40 @@
 
 @implementation Msg_Forward_Class
 
-//+ (BOOL)resolveClassMethod:(SEL)sel
-//{
-//    NSLog(@"%s 他 %@ 来了",__FUNCTION__, NSStringFromSelector(sel));
-////    if (sel == @selector(testClassClassMethod)) {
-////        IMP sayHIMP = class_getMethodImplementation(objc_getMetaClass("TestClass"), @selector(sayHappay));
-////        Method sayHMethod = class_getClassMethod(objc_getMetaClass("TestClass"), @selector(sayHappay));
-////        const char *sayHType = method_getTypeEncoding(sayHMethod);
-////        return class_addMethod(objc_getMetaClass("TestClass"), sel, sayHIMP, sayHType);
-////    }
-//    return [super resolveClassMethod:sel];
-//}
-//
-//+ (BOOL)resolveInstanceMethod:(SEL)sel
-//{
-//    NSLog(@"%s 他 %@ 来了",__FUNCTION__, NSStringFromSelector(sel));
-//    return [super resolveInstanceMethod:sel];
-//}
++ (BOOL)resolveClassMethod:(SEL)sel
+{
+    NSLog(@"%s  %@ 来了",__FUNCTION__, NSStringFromSelector(sel));
+//    if (sel == @selector(testClassClassMethod)) {
+//        IMP sayHIMP = class_getMethodImplementation(objc_getMetaClass("TestClass"), @selector(sayHappay));
+//        Method sayHMethod = class_getClassMethod(objc_getMetaClass("TestClass"), @selector(sayHappay));
+//        const char *sayHType = method_getTypeEncoding(sayHMethod);
+//        return class_addMethod(objc_getMetaClass("TestClass"), sel, sayHIMP, sayHType);
+//    }
+    return [super resolveClassMethod:sel];
+}
+
++ (BOOL)resolveInstanceMethod:(SEL)sel
+{
+    NSLog(@"%s  %@ 来了",__FUNCTION__, NSStringFromSelector(sel));
+    return [super resolveInstanceMethod:sel];
+}
 
 - (id)forwardingTargetForSelector:(SEL)aSelector
 {
+    NSLog(@"%s  %@ 来了",__FUNCTION__, NSStringFromSelector(aSelector));
     return [super forwardingTargetForSelector:aSelector];
+}
+
+- (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector
+{
+    NSLog(@"%s  %@ 来了",__FUNCTION__, NSStringFromSelector(aSelector));
+    return [super methodSignatureForSelector:aSelector];
+}
+
+- (void)forwardInvocation:(NSInvocation *)anInvocation
+{
+    NSLog(@"%s  %@ 来了",__FUNCTION__, NSStringFromSelector(anInvocation.selector));
+    return [super forwardInvocation:anInvocation];
 }
 
 - (void)abc
